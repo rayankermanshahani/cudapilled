3ba1d9bfcf99b522a2a77319b710efd35c22365c
rayankermanshahani <rayan.k@gmx.com>
2024-11-13 08:59:25 -0500

    display some more properties

--- 03_device_query/dev_query.cu
+++ 03_device_query/dev_query.cu
@@ -22,14 +22,22 @@ int main(void) {
     fprintf(stdout, "  name: %s\n", deviceProperties.name);
     fprintf(stdout, "  major compute capability: %d\n", deviceProperties.major);
     fprintf(stdout, "  minor compute capability: %d\n", deviceProperties.minor);
+    fprintf(stdout, "  total global memory: %lf GB\n",
+            (double)deviceProperties.totalGlobalMem / (1024 * 1024 * 1024));
+    fprintf(stdout, "  total constant memory: %lf GB\n",
+            (double)deviceProperties.totalConstMem / (1024 * 1024 * 1024));
     fprintf(stdout, "  warp size: %d\n", deviceProperties.warpSize);
     fprintf(stdout, "  clock rate: %d kHz\n", deviceProperties.clockRate);
     fprintf(stdout, "  max threads per block: %d\n",
             deviceProperties.maxThreadsPerBlock);
+    fprintf(stdout, "  max registers per block: %d\n",
+            deviceProperties.regsPerBlock);
     fprintf(stdout, "  total number of SMs: %d\n",
             deviceProperties.multiProcessorCount);
     fprintf(stdout, "  max threads per SM: %d\n",
             deviceProperties.maxThreadsPerMultiProcessor);
+    fprintf(stdout, "  max registers per SM: %d\n",
+            deviceProperties.regsPerMultiprocessor);
     fprintf(stdout, "  dimension z:\n    max blocks: %d\n    max threads: %d\n",
             deviceProperties.maxGridSize[2], deviceProperties.maxThreadsDim[2]);
     fprintf(stdout, "  dimension y:\n    max blocks: %d\n    max threads: %d\n",
